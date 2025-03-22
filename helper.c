2ed438a5957b57feda34c7323cc199e591ac3e13
Yassine  Aitmouloud <yaaitmou@c3r1p4.1337.ma>
2025-03-22 00:20:04 +0000

    whyy

--- helper.c
+++ helper.c
@@ -3,14 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   helper.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: souel-bo <souel-bo@student.42.fr>          +#+  +:+       +#+        */
+/*   By: yaaitmou <yaaitmou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 15:50:25 by souel-bo          #+#    #+#             */
-/*   Updated: 2025/03/20 15:51:49 by souel-bo         ###   ########.fr       */
+/*   Updated: 2025/03/22 00:17:57 by yaaitmou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "LIBFT/libft.h"
 #include "minishell.h"
+
 
 void	ft_free_split(char **split)
 {
@@ -23,4 +25,22 @@ void	ft_free_split(char **split)
 		i++;
 	}
 	free(split);
+}
+int	is_builtin(char *cmd, char **envp, t_global *input)
+{
+	if (!ft_strncmp(cmd, "echo", 4))
+		ft_echo(input);
+	if (!ft_strncmp(cmd, "env", 3))
+		ft_env(envp);	
+	if (!ft_strncmp(cmd, "cd", 2))
+		ft_chdir(input);
+	if (!ft_strncmp(cmd, "exit", 4))
+		ft_exit(input);
+	if (!ft_strncmp(cmd, "pwd", 3))
+		ft_pwd();
+	if (!ft_strncmp(cmd, "export", 6))
+		return (1);
+	if (!ft_strncmp(cmd, "unset", 5))
+		return (1);
+	return (0);
 }
